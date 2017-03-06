2386b8874be265bb7d3b17cf771b95b7d55a1e74
Marko Draksic <marko@savant.no>
2017-03-06 22:56:23 +0100

    Limiting the click area for closing the success message

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,14 @@
+var alertClose = document.getElementById('message-close');
 var alertMessage = document.getElementById('message-alert');
 var messageInput = document.getElementById('message-input');
 
-// add event listener to alert
+// add event listener to the success alert close link
 //hide it on click
-alertMessage.addEventListener('click', function() {
-    this.style.display = 'none';
+alertClose.addEventListener('click', function() {
+    alertMessage.style.display = 'none';
 });
 
-
+//on key up
 var inputValidation = function () {
     var warning = document.getElementById('message-warning');
     var button = document.getElementById('message-button');
@@ -19,6 +20,7 @@ var inputValidation = function () {
     button.disabled = length <= 8;
 }
 
+//show success message on submit
 var successMessage = function () {
    messageInput.value = '';
    alertMessage.style.display = 'block';
